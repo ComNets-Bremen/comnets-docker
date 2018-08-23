d8f58831a1e15ba36437029e5a4421d157e04fff
Jens Dede <jd@comnets.uni-bremen.de>
2018-08-23 14:27:20 +0200

    rename simu script

---
+++ ops-hb-13-eval/run-local-simu.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker run -it -v /home/jd/src/comnets-github/comnets-docker/ops-hb-13-eval/results:/opt/OPS/simulations/out ops-hb-13-eval:latest
+
+#docker run -it ops-hb-13-eval:latest
+
+
