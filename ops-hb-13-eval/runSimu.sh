7b53b55822f1ed6d25b028ee7f5494cecbfcea18
Jens Dede <jd@comnets.uni-bremen.de>
2018-08-23 16:09:54 +0200

    renamed script

--- ops-hb-13-eval/runSimu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run -it -v /home/jd/src/comnets-github/comnets-docker/ops-hb-13-eval/results:/opt/OPS/simulations/out ops-hb-13-eval:latest
-
-#docker run -it ops-hb-13-eval:latest
-
-
